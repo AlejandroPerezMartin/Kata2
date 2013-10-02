dc319380222a189c8f906e69474c7662f121be4d
alejandro <alejandro@localhost.localdomain>
2013-10-02 23:08:32 +0100

    Return the most frequent number in an array

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -4,25 +4,35 @@ import java.util.HashMap;
 
 public class Kata2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         
-        int[] vector = {1,7,2,4,6,3,2,8,0,1,1,1,5,8,9,0,3,5,7,7,9};
+        HashMap<Integer, Integer> numbersList = new HashMap<>();
+        int[] vector = { 1,7,2,4,6,3,2,8,0,1,1,1,5,8,9,0,3,5,7,7,9,8,8 };        
+        int mostFrequentNumber = 0;
+        int maxOccurrences = 1;
         
-        HashMap<Integer, Integer> list = new HashMap<>();
-        
-        for( Integer currentValue : vector ){
+        for( Integer currentVectorValue : vector ){
             
-            if( list.containsKey( currentValue ) ){
-                int occurrences = list.get( currentValue );
-                list.put( currentValue, ++occurrences );
+            if( numbersList.containsKey( currentVectorValue ) )
+            {
+                
+                int currentOccurrences = numbersList.get( currentVectorValue );
+                numbersList.put( currentVectorValue, ++currentOccurrences );
+                
+                if( currentOccurrences >= maxOccurrences )
+                {
+                    mostFrequentNumber = currentVectorValue;
+                    maxOccurrences = currentOccurrences;
+                }
+                
             }
-            else{
-                list.put( currentValue, 1 );
-            }
+            else
+                numbersList.put( currentVectorValue, 1 );
            
         }
         
-        System.out.println( list );
+        System.out.println( "The most frequent number is: "+mostFrequentNumber );
         
     }
     
